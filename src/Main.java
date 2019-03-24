b62112f064989767ad3c009125ecdfaafba091d6
NADOPODUMAT <48878930+NADOPODUMAT@users.noreply.github.com>
2019-03-24 12:30:31 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        int[] arr = new int[5]
+        ;
+        //for (int i = 0; i < 5; i++) {
+        //    arr[i] = i;
+       // }
+       // System.out.println(Arrays.toString(arr));
 
+        int j = 0;
+
+        while (j <5){
+            arr[j] = j++;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
